80633f284099f1ead20186d7056fbfc5b5d5b1fc
Binh Tran <binhpv80@gmail.com>
2023-10-12 12:36:48 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const soap = require('soap');
+const SERVICE_WSDL = process.env.SERVICE_WSDL || "http://ip-172-31-100-162.ap-southeast-1.compute.internal/ws/countries.wsdl";
+
+const transformRequest = async(event)=>{
+    let targetRequest = {};
+    targetRequest.name = event.countryName; 
+    //targetRequest.intB = event.b;
+    return targetRequest;
+};
+
+const transformResponse = async(targetResponse)=>{
+    let sourceResponse = targetResponse;
+    return sourceResponse;
+};
+
+const callSOAPService = async(targetRequest, wsdl)=>{
+    return new Promise((resolve, reject)=>{
+
+        var options = {
+            forceSoap12Headers: false // Required if soap12 bindings are to be used
+        };
+        
+        soap.createClient(wsdl, options, function(err, client) {
+            if(err){
+                reject(err);
+            }
+            let t1 = new Date().getTime();
+            console.log(wsdl);
+            // Call the actual SOAP operation as defined in the WSDL
+            //client.describe();
+            client.getCountry(targetRequest, function(err, result) {
+                if(err){
+                    reject(err);
+                }
+                else{
+                    let t2 = new Date().getTime();
+                    console.log("Service call returned in ", (t2-t1), " msecs");
+                    resolve(result);
+                }
+            });
+        });
+        
+    });  
+};
+
+exports.handler = async(event)=>{
+    console.log("Event >> ", event);
+
+    try{
+        // Transform request
+        let transformedReq = await transformRequest(event).catch((err)=>{
+            console.error("Error while transforming request >> ", err);
+            throw Error('Request transformation error');
+        });
+    
+        // Place SOAP service call
+        let targetResponse = {};
+        await callSOAPService(transformedReq, SERVICE_WSDL).then((result)=>{
+            console.log("Result >> ", result);
+            targetResponse = result;
+        }).catch((err)=>{
+            console.error("Error during SOAP call >> ", err);        
+            throw Error('Service call error');
+        });
+    
+        // Transform response
+        let transformedResp = await transformResponse(targetResponse).catch((err)=>{
+            console.error("Error while transforming response >> ", err);
+            throw Error('Response transformation error');
+        });
+         console.log("Final response >> ", transformedResp);
+        return transformedResp;
+    }
+    catch(err){
+        return { error: err.message };   
+    }
+};
